8e50de19c8b8eae81cc91dcf8ce42ef9318c3ca0
Ismael Moura <ismaeldesousa2014@outlook.com>
2023-03-14 15:34:20 -0300

    OrderStatus enum

--- src/main/java/com/ismaelmoura/course/config/TestConfig.java
+++ src/main/java/com/ismaelmoura/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.ismaelmoura.course.config;
 
 import com.ismaelmoura.course.entities.Order;
 import com.ismaelmoura.course.entities.User;
+import com.ismaelmoura.course.entities.enums.OrderStatus;
 import com.ismaelmoura.course.respositories.OrderRepository;
 import com.ismaelmoura.course.respositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,9 +36,9 @@ public class TestConfig implements CommandLineRunner {
                 "977777777",
                 "123456");
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
 
         userRepository.saveAll(Arrays.asList(user1, user2));
